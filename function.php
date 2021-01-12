dc3463a47f50b65c6156697a1aa2413dec6b603b
SimonSiksta420 <simon.siksta@student.ossp.cz>
2021-01-12 16:36:12 +0100

    added planets

--- function.php
+++ function.php
@@ -5,7 +5,7 @@ function drawMonster(int $type, int $size):void {
 ?>
 
 
- <img src="floppa-<?= $type; ?>.jpg" style="width: <?= $size; ?>px; height: <?= $size; ?>px;"> 
+ <img src="planet<?= $type; ?>.jpg" style="width: <?= $size; ?>px; height: <?= $size; ?>px;"> 
 
 
 <?php
